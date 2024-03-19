9d045f897e4d2fbb97af505e7c9996257e9f8eb6
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-03-19 01:50:17 -0500

    Added indexController.js to handle root route

--- Server/Controllers/AppController.js
+++ Server/Controllers/AppController.js
@@ -4,12 +4,14 @@ const path = require('path')
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const indexController = require('./indexController');
 const getRocketLeagueData = require('./rocketLeagueController')
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, '../../public/pages'))
 
 app.use(express.static(path.resolve(__dirname, '../../public')))
+app.use('/', indexController);
 
 app.get('/rocket-league', getRocketLeagueData)
 
