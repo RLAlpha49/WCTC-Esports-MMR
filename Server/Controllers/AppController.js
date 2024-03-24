5a23d6fef0ea2b03c01090e7e7bcc2173c37ce3b
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-03-23 22:13:36 -0500

    Add rateLimiter.js

--- Server/Controllers/AppController.js
+++ Server/Controllers/AppController.js
@@ -1,18 +1,18 @@
-// noinspection JSCheckFunctionSignatures
-
 const path = require('path')
 const express = require('express')
 const app = express()
 require('dotenv').config()
 const indexController = require('./indexController')
 const getRocketLeagueData = require('./rocketLeagueController')
+const limiter = require('../rateLimiter')
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve(__dirname, '../../public/pages'))
 
 app.use(express.static(path.resolve(__dirname, '../../public')))
-app.use('/', indexController)
 
-app.get('/rocket-league', getRocketLeagueData)
+app.use('/', limiter, indexController)
+
+app.use('/rocket-league', limiter, getRocketLeagueData)
 
 module.exports = app
---
+++ Server/rateLimiter.js
@@ -0,0 +1,8 @@
+const RateLimit = require('express-rate-limit')
+
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+})
+
+module.exports = limiter
